d32126413bfc847b2563733d8d8baffcc7a99faf
Tarik Rashada <trashada1@gmail.com>
2025-08-07 00:29:01 -0400

    feat: Implementation of AstPrinter - concrete ExprVisitor[any, string] interface

--- parser/ast.go
+++ parser/ast.go
@@ -1,55 +1,91 @@
 package parser
 
 import (
+	"fmt"
 	"github/goInterpreter/lexer"
+	"strings"
 )
 
 // generic Expr[T] with generic Accept method
 // generic ExprVisitor[T] interface with VisitX
 // for every concrete Expr[T] (nodes)
 
-type ExprVisitor[T any] interface {
-	VisitBinary(*Binary[T]) T
-	VisitUnary(*Unary[T]) T
-	VisitGrouping(*Grouping[T]) T
-	VisitLiteral(*Literal[T]) T
+type ExprVisitor[T, V any] interface {
+	VisitBinary(*Binary[T, V]) V
+	VisitUnary(*Unary[T, V]) V
+	VisitGrouping(*Grouping[T, V]) V
+	VisitLiteral(*Literal[T, V]) V
 }
 
-type Expr[T any] interface {
-	Accept(ExprVisitor[T]) T
+type Expr[T, V any] interface {
+	// T: the type of literal values
+	// V: the return type of the visitor
+	Accept(ExprVisitor[T, V]) V
 }
 
-type Binary[T any] struct {
-	Left     Expr[T]
+type Binary[T, V any] struct {
+	Left     Expr[T, V]
 	Operator lexer.Token
-	Right    Expr[T]
+	Right    Expr[T, V]
 }
 
-func (bin *Binary[T]) Accept(visitor ExprVisitor[T]) T {
+func (bin *Binary[T, V]) Accept(visitor ExprVisitor[T, V]) V {
 	return visitor.VisitBinary(bin)
 }
 
-type Unary[T any] struct {
+type Unary[T, V any] struct {
 	Operator lexer.Token
-	Right    Expr[T]
+	Right    Expr[T, V]
 }
 
-func (un *Unary[T]) Accept(visitor ExprVisitor[T]) T {
+func (un *Unary[T, V]) Accept(visitor ExprVisitor[T, V]) V {
 	return visitor.VisitUnary(un)
 }
 
-type Grouping[T any] struct {
-	Expression Expr[T]
+type Grouping[T, V any] struct {
+	Expression Expr[T, V]
 }
 
-func (gr *Grouping[T]) Accept(visitor ExprVisitor[T]) T {
+func (gr *Grouping[T, V]) Accept(visitor ExprVisitor[T, V]) V {
 	return visitor.VisitGrouping(gr)
 }
 
-type Literal[T any] struct {
+type Literal[T, V any] struct {
 	Value T
 }
 
-func (l *Literal[T]) Accept(visitor ExprVisitor[T]) T {
+func (l *Literal[T, V]) Accept(visitor ExprVisitor[T, V]) V {
 	return visitor.VisitLiteral(l)
+}
+
+type AstPrinter struct{}
+
+// printer should return a string so it implementst the Expr[T=string] interface
+func (astp *AstPrinter) VisitBinary(bin *Binary[any, string]) string {
+	return printHelper(astp, bin.Operator.Lexeme, bin.Left, bin.Right)
+}
+
+func (astp *AstPrinter) VisitLiteral(lit *Literal[any, string]) string {
+	if lit.Value == nil {
+		return "nil"
+	}
+	return fmt.Sprint(lit.Value)
+}
+func (astp *AstPrinter) VisitGrouping(gr *Grouping[any, string]) string {
+	return printHelper(astp, "group", gr.Expression)
+}
+func (astp *AstPrinter) VisitUnary(un *Unary[any, string]) string {
+	return printHelper(astp, un.Operator.Lexeme, un.Right)
+}
+
+func printHelper(astp ExprVisitor[any, string], operation string, exprArgs ...Expr[any, string]) string {
+	sb := strings.Builder{}
+	sb.WriteString("(")
+	sb.WriteString(operation)
+	for _, exp := range exprArgs {
+		sb.WriteString(" ")
+		sb.WriteString(exp.Accept(astp))
+	}
+	sb.WriteString(")")
+	return sb.String()
 }
